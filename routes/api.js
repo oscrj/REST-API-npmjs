8f16a5e777a53b114106b1a0d67ccd344b84daf0
Oscar Johanneson <oscar.johanneson@gmail.com>
2020-12-09 16:47:45 +0100

    add getQute through params

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const qoutes = require('find-quote');
 
 router.get('/', (req,res) => {
-    res.send('Test /api')
+    //res.sendFile('quotes.html',{root: __dirname})
+    res.send('<section style="margin: 200px;"><div style="width:100%; margin 100px; text-align: center;"><h1>Quotes</h1></div> </section>');
 });
 
+router.get('/quotes/:quote', (req,res) => {
+    let quoteFound = qoutes.getQuote(req.params.quote);
+    let isFound = quoteFound === "" ? 'No quotes were found with the word ' + req.params.quote : quoteFound
+
+    res.send(
+    `<section style="margin: 200px;">
+        <div style="width:100%; margin 100px; text-align: center;">
+            <h2>`+ isFound +`</h2>
+        </div>
+    </section>`
+    );
+});
 
 module.exports = router;
