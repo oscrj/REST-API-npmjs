f13fa35162c6d0a8cbcb34d5f8cfc2a60fa1649e
Oscar Johanneson <oscar.johanneson@gmail.com>
2020-12-09 20:56:55 +0100

    add POST method to add user

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const qoutes = require('find-quote');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const { json } = require('body-parser');
+
+router.use(bodyParser.json());
 
 router.get('/', (req,res) => {
     //res.sendFile('quotes.html',{root: __dirname})
@@ -10,14 +15,43 @@ router.get('/', (req,res) => {
 router.get('/quotes/:quote', (req,res) => {
     let quoteFound = qoutes.getQuote(req.params.quote);
     let isFound = quoteFound === "" ? 'No quotes were found with the word ' + req.params.quote : quoteFound
-
+    
     res.send(
-    `<section style="margin: 200px;">
-        <div style="width:100%; margin 100px; text-align: center;">
-            <h2>`+ isFound +`</h2>
-        </div>
-    </section>`
+        `<section style="margin: 200px;">
+            <div style="width:100%; margin 100px; text-align: center;">
+                <h2>`+ isFound +`</h2>
+            </div>
+        </section>`
     );
 });
+
+router.get('/user', (req, res) => {    
+    res.send('User');
+});
+
+
+router.post('/user/add/:name', (req, res) => {
+    
+    let name = req.params.name;
+    //let age = req.params.age;
+    // // let userId = req.params.userId;
+    // res.status(200).json({
+    //     name
+    // })
+
+    let body = { 
+        name: name,
+        age: age
+    };
+
+    fetch('https://httpbin.org/post', {
+            method: 'POST',
+            body:    JSON.stringify(body),
+            headers: { 'Content-Type': 'application/json' },
+        })
+        .then(res => res.json())
+        .then(json => res.send(json.json));
+});
+
 
 module.exports = router;
