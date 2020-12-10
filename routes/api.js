6ed3b6981fb3e436f2c7f874e3928200bd829eea
Oscar Johanneson <oscar.johanneson@gmail.com>
2020-12-10 18:03:02 +0100

    Add params to POST method

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,10 @@ const router = express.Router();
 const qoutes = require('find-quote');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-const { json } = require('body-parser');
 
 router.use(bodyParser.json());
 
 router.get('/', (req,res) => {
-    //res.sendFile('quotes.html',{root: __dirname})
     res.send('<section style="margin: 200px;"><div style="width:100%; margin 100px; text-align: center;"><h1>Quotes</h1></div> </section>');
 });
 
@@ -23,35 +21,32 @@ router.get('/quotes/:quote', (req,res) => {
             </div>
         </section>`
     );
-});
-
-router.get('/user', (req, res) => {    
-    res.send('User');
-});
-
-
-router.post('/user/add/:name', (req, res) => {
     
-    let name = req.params.name;
-    //let age = req.params.age;
-    // // let userId = req.params.userId;
-    // res.status(200).json({
-    //     name
-    // })
+    // Om du vill se resultatet i json-format. 
+    // let body = {
+    //     quote: isFound
+    // }
+    // res.json(body)
+});
 
-    let body = { 
-        name: name,
-        age: age
+router.get('/user/add/:username/:firstname/:lastname', (req, res) => {
+    let username = req.params.username;
+    let firstname = req.params.firstname;
+    let lastname = req.params.lastname;
+    
+    let user = { 
+        username: username,
+        firstname: firstname,
+        lastname: lastname 
     };
 
     fetch('https://httpbin.org/post', {
             method: 'POST',
-            body:    JSON.stringify(body),
+            body:    JSON.stringify(user),
             headers: { 'Content-Type': 'application/json' },
         })
         .then(res => res.json())
         .then(json => res.send(json.json));
 });
-
 
 module.exports = router;
